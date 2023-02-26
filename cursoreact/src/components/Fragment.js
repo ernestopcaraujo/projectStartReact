aec008b55cd3e170f472fd93524652224ea920fe
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-02-26 13:02:35 -0300

    feat: created component

---
+++ cursoreact/src/components/Fragment.js
@@ -0,0 +1,14 @@
+const Fragment = ({myString}) => {
+
+    return(
+        <>
+            <p>Fragment</p>
+            <p>Teste</p>
+            <p>API</p>
+            <h2>teste</h2>
+            <h2>{myString}</h2> 
+
+        </>
+    );
+}
+export default Fragment;
