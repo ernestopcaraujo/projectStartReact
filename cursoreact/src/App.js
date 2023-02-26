557347ac4c4b22f545f39661bca85ed062d9fea5
ernestopcaraujo@gmail.com <ernestopcaraujo@gmail.com>
2023-02-26 13:01:29 -0300

    feat: added new components

--- cursoreact/src/App.js
+++ cursoreact/src/App.js
@@ -3,6 +3,10 @@ import FirstComponent from './components/FirstComponent';
 import AnotherComponent from './components/AnotherComponent';
 import Images from './components/Images';
 import Hooks from './components/Hooks';
+import List from './components/List';
+import RenderCond from './components/RenderCond';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
 
 function App() {
   return (
@@ -12,6 +16,12 @@ function App() {
       <AnotherComponent />
       <Images />
       <Hooks />
+      <List />
+      <RenderCond x={100} y={10} />
+      <Fragment myString="UOOOOOO" />
+      <Container>
+        <h2>Teste do Container</h2>
+      </Container>
     </div>
   );
 }
